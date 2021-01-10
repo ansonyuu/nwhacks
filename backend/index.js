35f215a07d9f03f9ce60900b6cfbe79524c42ffe
Lucy <hao.lucyy@gmail.com>
2021-01-10 12:55:23 -0700

    add logs

--- backend/index.js
+++ backend/index.js
@@ -1,59 +1,59 @@
-const express = require("express");
+// const express = require("express");
 
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "0.0.0.0";
+// const PORT = process.env.PORT || 3000;
+// const HOST = process.env.HOST || "0.0.0.0";
 
-const { declareDatabaseRoutes } = require("./api/declareUserRoutes");
-const { declareMatchRoutes } = require("./api/declareMatchRoutes")
+// const { declareDatabaseRoutes } = require("./api/declareUserRoutes");
+// const { declareMatchRoutes } = require("./api/declareMatchRoutes")
 
-const app = express();
+// const app = express();
 
-app.promiseListen = function promiseListen(port, host) {
-  return new Promise((resolve, reject) =>
-    this.listen(port, host, (err) => (!err ? resolve() : reject(err)))
-  );
-};
+// app.promiseListen = function promiseListen(port, host) {
+//   return new Promise((resolve, reject) =>
+//     this.listen(port, host, (err) => (!err ? resolve() : reject(err)))
+//   );
+// };
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+// const { v4: uuidV4 } = require('uuid');
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+// app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+// app.get('/', (req, res) => {
+//   res.redirect(`/${uuidV4()}`)
+// })
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+// app.get('/:room', (req, res) => {
+//   res.render('room', { roomId: req.params.room })
+// })
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+// io.on('connection', socket => {
+//   socket.on('join-room', (roomId, userId) => {
+//     socket.join(roomId)
+//     socket.to(roomId).broadcast.emit('user-connected', userId)
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+//     socket.on('disconnect', () => {
+//       socket.to(roomId).broadcast.emit('user-disconnected', userId)
+//     })
+//   })
+// })
 
-server.listen(3000);
+// server.listen(3000);
 
-const runServer = async () => {
-  // declare routes
-  declareDatabaseRoutes(app);
-  declareMatchRoutes(app);
+// const runServer = async () => {
+//   // declare routes
+//   declareDatabaseRoutes(app);
+//   declareMatchRoutes(app);
 
 
-  // start server
-  await app.promiseListen(PORT, HOST);
-  console.log(`[Server Startup] Running on http://${HOST}:${PORT}`);
-};
+//   // start server
+//   await app.promiseListen(PORT, HOST);
+//   console.log(`[Server Startup] Running on http://${HOST}:${PORT}`);
+// };
 
-module.exports = {
-  app,
-  runServer,
-};
+// module.exports = {
+//   app,
+//   runServer,
+// };
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,54 @@
 const { runServer } = require('./index');
 
-runServer();
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+
+const { declareDatabaseRoutes } = require("./api/declareUserRoutes");
+const { declareMatchRoutes } = require("./api/declareMatchRoutes")
+
+const app = express();
+
+app.promiseListen = function promiseListen(port, host) {
+  return new Promise((resolve, reject) =>
+    this.listen(port, host, (err) => (!err ? resolve() : reject(err)))
+  );
+};
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+declareDatabaseRoutes(app);
+declareMatchRoutes(app);
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+    console.log("new user")
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      console.log("left")
+    })
+  })
+})
+
+server.listen(PORT);
+
+
+
+
